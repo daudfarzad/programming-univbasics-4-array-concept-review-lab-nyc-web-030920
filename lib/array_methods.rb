795f8272b40c180171461be22004b9543f43fedc
Farzad Daud <daud.farzad1@gmail.com>
2020-03-02 16:22:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
- array.length.times do |count|
-   if array[count] == value_to_find
-     return count 
+ array.length.times do |index|
+   if array[index] == value_to_find
+     return index 
    end
    nil
  end
@@ -9,17 +9,18 @@ end
    
 def find_max_value(array)
   x = 0 
-  array.length.times { |index| x = arrat[index] if array[index] > x }
+  array.length.times { |index| x = array[index] if array[index] > x }
   x
 end
+
 def find_min_value(array)
-  x = array [o]
+  x = array[0]
   array.length.times do |index|
     if arrat[index] < x 
-      x = array [index]
+      x = array[index]
     end 
   end 
   x
 end
 
-end
+
